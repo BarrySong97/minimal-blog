4c18489cf0777b220bf14ff654b2fbfba4773075
Barry Song <524000659@qq.com>
2025-02-23 10:32:31 +0800

    feat(调整home): 布局

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -4,8 +4,8 @@ import { Projects } from "@/components/home/Projects";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background">
-      <main className="container max-w-6xl mx-auto px-6 xl:px-0 py-12">
+    <div className=" bg-background">
+      <main className="container max-w-6xl mx-auto px-6 xl:px-0 pt-6">
         <div className="space-y-16">
           <Profile />
           <div className="grid grid-cols-1 lg:grid-cols-[2fr,1fr] gap-24">
