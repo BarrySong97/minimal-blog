78e6e8c00c06bf3a1e80e9c55c13e1f75a2240a6
Barry Song <524000659@qq.com>
2025-03-23 17:12:16 +0800

    feat(viewtransition): remove view transition

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
 
 const config: NextConfig = {
-  experimental: {
-    viewTransition: true,
-  },
   images: {
     minimumCacheTTL: 2678400,
     remotePatterns: [
